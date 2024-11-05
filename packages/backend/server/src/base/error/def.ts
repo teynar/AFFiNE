574a22cb6cdbb22f41d66137f10382d1235259b4
DarkSky <darksky2048@gmail.com>
2024-11-05 14:25:13 +0800

    feat: update deps

--- packages/backend/server/src/base/error/def.ts
+++ packages/backend/server/src/base/error/def.ts
@@ -235,6 +235,10 @@ export const USER_FRIENDLY_ERRORS = {
     type: 'resource_not_found',
     message: 'Resource not found.',
   },
+  bad_request: {
+    type: 'bad_request',
+    message: 'Bad request.',
+  },
 
   // User Errors
   user_not_found: {
